db0e22495abc043584a40a4edeb1a344c1a990f8
jackdude25 <jack.aaron2727@gmail.com>
2021-08-10 10:42:19 -0400

    add delete

--- public/javascripts/create.js
+++ public/javascripts/create.js
@@ -1,3 +1,79 @@
+document.getElementById("submit").onclick=function(evt){
+  evt.preventDefault();
+  const form = document.querySelector("form")
+  const formData = new FormData(document.querySelector("form"));
+  const isValid = form.checkValidity();
+
+  if (!isValid) {
+    var err = {
+      response: {
+        data: [
+          {
+            field: "name",
+            message: "Name is required",
+          },
+        ],
+      },
+    };
+
+    handleErrors(err);
+    
+    return;
+  }
+  if (formData.get("name").length > 25){
+    var err = {
+      response: {
+        data: [
+          {
+            field: "name",
+            message: "Name is greater than 25 characters",
+          },
+        ],
+      },
+    };
+
+    handleErrors(err);
+    
+    return;
+  }
+  if (formData.get("price") <= 0){
+    var err = {
+      response: {
+        data: [
+          {
+            field: "price",
+            message: "Price is invalid",
+          },
+        ],
+      },
+    };
+
+    handleErrors(err);
+    
+    return;
+  }
+  
+  axios.post("/api/products", {
+    name: formData.get("name"),
+    price: formData.get("price"),
+    quantity: formData.get("quantity"),
+    description: formData.get("description"),
+    color: formData.get("color"),
+  }).then(processResults).catch(handleErrors);
+}
+function handleErrors({ response }) {
+  const errorElements = document.getElementsByClassName("error");
+  for (let i = 0; i < errorElements.length; i++) {
+    errorElements[i].textContent = "";
+  }
+
+  const errors = response.data;
+  for (let i = 0; i < errors.length; i++) {
+    const { field, message } = errors[i];
+    const element = document.getElementsByName(field)[0].nextElementSibling;
+    element.textContent = message;
+  }
+}
 function processResults({ data }) {
   document.querySelector("form").reset();
   window.alert(`${data.name} added with id: ${data.id}`);
--- public/javascripts/delete.js
+++ public/javascripts/delete.js
@@ -1,3 +1,9 @@
+document.getElementById("delete").onclick = function(){
+  const value = document.getElementById("product-id").value;
+  axios.delete(`/api/products/${value}`).then(processResult);
+}
+  
+
 function processResult() {
   window.alert("Product deleted!");
 }
